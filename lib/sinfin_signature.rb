65e9609e858abd5ccd87a156f57fab95b1b2dfa9
Petr Marek <contact@petrmarek.eu>
2021-02-05 12:51:12 +0100

    update styles and helper loader

--- lib/sinfin_signature.rb
+++ lib/sinfin_signature.rb
@@ -5,6 +5,11 @@ require 'sinfin_signature/helper'
 module SinfinSignature
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'sinfin_signature_helper.action_controller' do
+        ActiveSupport.on_load :action_controller do
+          helper SinfinSignature::Helper
+        end
+      end
     end
   end
 end
